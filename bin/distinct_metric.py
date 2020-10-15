9c85ef60eab613a418232ca6803241617e60fd0a
Yu Yan <yuyan2do@gmail.com>
2020-10-14 17:33:32 -0700

    Update distinct_metric.py

--- bin/distinct_metric.py
+++ bin/distinct_metric.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('hypothesis', help="predicted text file, one example per line")
     parser.add_argument('-n', dest='n_range', type=int, nargs='+', help="n to use as in distinct-N")
-    parser.add_argument('--output_dir')
+    #parser.add_argument('--output_dir')
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.WARNING)
@@ -20,6 +20,6 @@ if __name__ == '__main__':
     with open(args.hypothesis) as f:
         hypothesis = ' '.join([line.strip() for line in f.readlines()])
 
-    output_dir = Path(args.output_dir)
+    #output_dir = Path(args.output_dir)
     for n in args.n_range:
         print(distinct_n_sentence_level(hypothesis, n))
